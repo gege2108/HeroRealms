db3a3d41b1a96d73196ff84b5fc985fc659c3786
khanh-lam <khanhlamduchet@gmail.com>
2025-10-23 22:28:00 +0200

    Desription de mes modifications

--- include/Action.h
+++ include/Action.h
@@ -14,10 +14,40 @@ public:
     Action() = default;
     Action(const std::string& nom) : Carte(nom) {}
 
+    //constructeur sans choix
+    Action(Faction faction, const std::string& nom, int prix, const std::vector<Effet>& effets1, const std::vector<EffetTextuel>& effetsTextuels1, const std::vector<Effet>& effets2, const std::vector<EffetTextuel>& effetsTextuels2) 
+        : Carte(nom, effets1) {
+        this->ListEffetTextuelChoix1 = effetsTextuels1;
+
+        this->ListEffetBasiqueCombo = effets2;
+        this->ListEffetTextuelCombo = effetsTextuels2;
+
+        this->faction = faction;
+        this->prix = prix;
+    }
+
+    //constructeur avec choix
+    Action(Faction faction, const std::string& nom, int prix, const std::vector<Effet>& effets1, const std::vector<EffetTextuel>& effetsTextuels1, const std::vector<Effet>& effets2, const std::vector<EffetTextuel>& effetsTextuels2, 
+        const std::vector<Effet>& effets3, const std::vector<EffetTextuel>& effetsTextuels3) 
+        : Carte(nom, effets1) {
+        this->ListEffetTextuelChoix1 = effetsTextuels1;
+
+        this->ListEffetBasiqueCombo = effets2;
+        this->ListEffetTextuelCombo = effetsTextuels2;
+
+        this->ListEffetBasiqueChoix2 = effets3;
+        this->ListEffetTextuelChoix2 = effetsTextuels3;
+
+        this->faction = faction;
+        this->prix = prix;
+    }
+
+
    // void utiliserEffet() override;
 
     int getPrix() const { return prix; }
     void setPrix(int p) { prix = p; }
+    void setPeutFaireCombo(bool b) { peutFaireCombo = b; }
 
     std::vector<Effet>& getListEffetBasiqueChoix2() { return ListEffetBasiqueChoix2; }
     std::vector<EffetTextuel>& getListEffetTextuelChoix1() { return ListEffetTextuelChoix1; }
---
+++ src/effetTextuel.cpp
@@ -0,0 +1,30 @@
+#include "EffetTextuel.h"
+
+// Comportement
+void EffetTextuel::executer() const {
+    // Aucun message à afficher — comportement par défaut : no-op
+}
+
+
+//1 Draw a card.
+//2 Target opponent discards a card.
+//3 Stun target champion.
+//4 Put the next action you acquire this turn on top of your deck.
+//5 Put the next card you acquire this turn into your hand.
+//6 You may draw a card, then discard a card.
+//7 You may draw up to two cards, then discard that many cards.
+//8 Take a champion from your discard pile and put it on top of your deck.
+//9 Take a card from your discard pile and put it on top of your deck.
+//10 You maysacrifice up to two cards in your hand and/or discard pile.
+//11 You may sacrifice a card in your hand or discard pile.
+//12 Prepare a champion.
+//13 +{1 combat} for each other guard you have in play.
+//14 +{1 combat} for each other {Wild} card you have in play.
+//15 +{1 combat} for each other champion you have in play.
+//16 +{2 combat} for each champion you have in play.
+//17 +{1 health} for each champion you have in play.
+//18 +{1 health} for each champion you have in play.
+//19 +{2 health} for each champion you have in play.
+
+//20 You may sacrifice a card in your hand or discard pile. If you do, gain an additional {3 combat}.
+//21 You may sacrifice a card in your hand or discard pile. If you do, gain an additional {2 combat}.

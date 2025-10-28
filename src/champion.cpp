c553e96d07ee14abc9fbc088a5506eac1a872b59
khanh-lam <khanhlamduchet@gmail.com>
2025-10-28 19:27:59 +0100

    ajout action chmapuion

---
+++ src/champion.cpp
@@ -0,0 +1,46 @@
+#include "Champion.h"
+
+/*
+Jaune
+Arkus, Dragon Impérial : 8g, 6pv, true
+Cristov, le Juste : 5g, 5pv, true
+Darian, Mage de Guerre : 4g, 5pv, true
+Kraka, Grand Prêtre : 6g, 6pv, false
+Homme d'Armes (2) : 3g, 4pv, true
+Maître Weyan : 4g, 4pv, true
+Percepteur de Dîme (2) : 2g, 3pv, false
+*/
+
+/*
+Bleu
+Borg, Mercenaire Ogre : 6g, 6pv, true
+Myros, Mage de la Guilde : 5g, 3pv, true
+Parov, l'Exécuteur : 5g, 5pv, true
+Rake, Maître Assassin : 7g, 7pv, true
+Rasmus, le Contrebandier : 4g, 5pv, false
+Bandit des Rues (2) : 3g, 4pv, false
+*/
+
+/*
+Rouge
+Prêtre du Culte (2) : 3g, 4pv, false
+Krythos, Maître Vampire : 7g, 6pv, false
+Lys, l'Inapparent : 6g, 5pv, true
+Rayla, Tisseuse de Fins : 4g, 4pv, false
+Tyrannor, le Dévoreur : 8g, 6pv, true
+Varrick, le Nécromancien : 5g, 3pv, false
+Cultiste de la Mort (2) : 2g, 3pv, true
+*/
+
+/*
+Vert
+Broelyn, Tisseuse de Savoirs : 4g, 6pv, false
+Cron, le Berserker : 6g, 6pv, false
+Loup Terrifiant : 5g, 5pv, true
+Grak, Géant de la Tempête : 8g, 7pv, true
+Grognard Orque (2) : 3g, 3pv, true
+Torgen Brise-Pierre : 7g, 7pv, true
+Shamane des Loups (2) : 2g, 4pv, false
+*/
+
+//27
